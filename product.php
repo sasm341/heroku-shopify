bd0f0140d253133454afdd6251bcf1e0abf929bf
sasm341 <79095498+sasm341@users.noreply.github.com>
2022-02-17 12:24:46 +0530

    Add files via upload

---
+++ product.php
@@ -0,0 +1,28 @@
+<?php
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://3000b7f64f7e5b19379c9e2d3df5e408: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@sasmita123.myshopify.com/admin/api/2021-10/products.json",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => array(
+    "accept: application/json",
+    "content-type: application/json"
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  print_r($response);
+}
+?>
