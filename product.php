3cb07470cbf50b23fff0d9ce5293c20361be8bb0
sasm341 <79095498+sasm341@users.noreply.github.com>
2022-02-17 13:10:18 +0530

    Update product.php

--- product.php
+++ product.php
@@ -1,4 +1,10 @@
 <?php
+$myfile = fopen("newfile.txt", "a") or die("Unable to open file!");
+$txt = "John Doe\n";
+fwrite($myfile, $txt);
+$txt = "Jane Doe\n";
+fwrite($myfile, $txt);
+fclose($myfile);
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
